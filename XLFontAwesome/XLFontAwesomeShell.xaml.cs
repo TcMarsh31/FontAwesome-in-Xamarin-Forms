71cd169de6e735e7a887dfff2e4745449ac22e7d
panchanathantharsan <tharsan.3112@gmail.com>
2020-10-23 11:41:59 +0530

    Initial check-in of module XLFontAwesome

---
+++ XLFontAwesome/XLFontAwesomeShell.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XLFontAwesome
+{
+    public partial class XLFontAwesomeShell : Shell
+    {
+        public XLFontAwesomeShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute("Page1", typeof(MainPage));
+            Routing.RegisterRoute("Page2", typeof(MainPage));
+
+        }
+    }
+}
